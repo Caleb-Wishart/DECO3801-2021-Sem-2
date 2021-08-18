9455dc755a8885cebab5f0296b4cb48ddecd6f37
Caleb-Wishart <caleb.wish@outlook.com>
2021-08-18 05:58:09 +0000

    update for all URL

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-# import sys
-# sys.path.append(1,'/var/www/uwsgi/branch/caleb')
 from werkzeug.middleware.dispatcher import DispatcherMiddleware 
 from controller import app as head
 from branch.caleb.controller import app as caleb
+from branch.adrian.controller import app as adrian
+from branch.alex.controller import app as alex
+from branch.jason.controller import app as jason
+from branch.matt.controller import app as matt
+from branch.kyle.controller import app as kyle
+
+
+
+
 application = DispatcherMiddleware(head, {
-        '/caleb': caleb
+        '/branch/caleb': caleb,
+        '/branch/adrian': adrian,
+        '/branch/alex': alex,
+        '/branch/jason': jason,
+        '/branch/matt': matt,
+        '/branch/kyle': kyle,
         })
