107d049cf152e8a27ddcb8e565439dfbf294d375
Caleb-Wishart <caleb.wish@outlook.com>
2021-08-19 07:54:30 +0000

    UI Overhaul and controller changes

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -6,43 +6,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    name = "example"
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    name = "Example User"
     if 'x-kvd-payload' in request.headers:
         userjson = request.headers['x-kvd-payload']
         user = json.loads(userjson)
         name = user['user']
-    name = "test"
-    return render_template('example.html',title='welcome', name=name)
+
+    data = [
+            {
+                'title':"Example 1",
+                'img':url_for('static', filename='img/placeholder.png')
+            },{
+                'title':"Example 2",
+                'img':url_for('static', filename='img/placeholder.png')
+            },{
+                'title':"Example 3",
+                'img':url_for('static', filename='img/placeholder.png')
+            },{
+                'title':"Example 4",
+                'img':url_for('static', filename='img/placeholder.png')
+            },{
+                'title':"Example 5",
+                'img':url_for('static', filename='img/placeholder.png')
+            }
+    ]
+
+    return render_template('home.html', title='Home', name=name,data=data)
 
 
-@app.route('/foobar')
-def foobar():
-    return "<span style='color:blue'>Hello again!</span>"
-
-# Caleb DMZ =====
-
-# Webpage for github webhook
-@app.route('/githook')
-def githook():
-    secret = request.headers['X-Hub-Signature']
-    parsed = json.loads(request.json)
-    with open('/home/s4585694/hook','w') as handle:
-        handle.write(json.dumps(parsed, indent=4, sort_keys=True))
-    return render_template('errors/error_404.html')
-    #return render_template('errors/error_404.html');
-
-# Caleb DMZ =====
-
-@app.route("/jason_test")
-def jtest():
-    return render_template("base.html", title="This is a test page")
-
-@app.route("/matthew_test")
-def cool_fun():
-    return render_template("matthew_wuz_here.html", test=name=="s4582166", user=name)
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us', name="About Us")
 
 
-@app.route("/kyle_test")
-def sexy_asian():
-    name = "Kyle Macaskill"
-    return render_template("kyle_is_really_funny.html", name=name, title = "Kyle")
+@app.errorhandler(403)
+def page_not_found(error):
+    return render_template('errors/error_403.html'), 403
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/error_404.html'), 404
