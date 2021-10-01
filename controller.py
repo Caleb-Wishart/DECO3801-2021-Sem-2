9ccbf7ce9e0460845c409bce261d98b4f68ee584
Kyle Macaskill <k.macaskill@uqconnect.edu.au>
2021-10-01 22:13:46 +1000

    Starting a single channel

--- controller.py
+++ controller.py
@@ -230,6 +230,13 @@ def channel(fName=None, tName=None):
     If fName is not valid name redirect to home forum page
     If tName is not valid redirect to forum page
     """
+    if fName is not None and tName is None:
+        return render_template('channel_item.html',
+                               title='Channels',
+                               subject=[enum_to_website_output(e) for e in Subject],
+                               grade=[enum_to_website_output(e) for e in Grade],
+                               tag=get_tags().keys(),
+                               resources=find_resources())
 
     if fName is None or tName is None:
         return render_template('channel.html',
