6dec4ee17a0dc65ecd5cffc87ac7a09e374990d7
s4581672 <s4581672@student.uq.edu.au>
2021-10-07 04:36:00 +0000

    FAQ page sortof working

--- controller.py
+++ controller.py
@@ -215,7 +215,13 @@ def settings():
 @app.route('/about')
 def about():
     """A brief page descibing what the website is about"""
-    return render_template('about.html', title='About Us', name="About Us")
+    faqs = [
+        ["Question 1","Hello world"],
+        ["Lorem Ipsum","Blah Blah Blah"],
+        ["What is Lorem Ipsum?","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
+        ["What is Doctrina?", "We are a teaching website for teachers by teachers"]
+    ]
+    return render_template('about.html', title='About Us', name="About Us", faqs=faqs, num=len(faqs))
 
 @app.route('/create')
 @app.route('/create/<type>')
